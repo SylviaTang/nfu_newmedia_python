7d7fe091e302aaa14dceea49128179f897c2dc82
Young Jiwaii <yaneziwaii@gmail.com>
2017-06-19 16:34:56 +0800

    修改版
    修改版

--- airportsweb/airportsweb.py
+++ airportsweb/airportsweb.py
@@ -1,19 +1,15 @@
  # -*- coding: utf-8 -*- 
-from flask import Flask, render_template, request, escape
-from calcudistance import get_distance
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    """Display this webapp's HTML form."""
     return render_template('entry.html',
                            the_title='欢迎来到网上查询国内两个机场之间的里程！')
 
 @app.route('/pickairport', methods=['POST'])
 def searchairports() -> 'html':
-    """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
     airportone = request.form['airportone']
     airporttwo = request.form['airporttwo']
     return render_template('results.html',
@@ -24,3 +20,17 @@ def searchairports() -> 'html':
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+import json
+with open('gaycone.json', 'r',encoding = 'utf8') as infile:
+    dddddd = json.load (infile)
+    dddddd.keys()
+    airportone = dddddd['']
+    airporttwo = dddddd['']
+
+def distance(a1,a2):
+    print(a1,a2)
+    d = a1['longitude']-a2['longitude']
+    return(d)
+    
+distance(airportone,airporttwo)
