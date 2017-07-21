d93dcf9790bcbf688e6074950a3b9029a91df99c
zdnfzgb <920697302@qq.com>
2017-07-21 16:13:03 +0800

    Update airportnames.py

--- airportsweb/airportnames.py
+++ airportsweb/airportnames.py
@@ -2,7 +2,7 @@
 
 class airport_list_name (object):
 
-    def __init__(self, fn='data\gaycone.json'):
+    def __init__(self, fn='data\Airports_zh_code_geo.json'):
        import json
        with open(fn, 'r', encoding='utf8') as infile:
            dd = json.load (infile)
