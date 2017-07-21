8dc39a8e4766b493631de8373715964a37128f87
zdnfzgb <920697302@qq.com>
2017-07-21 21:00:21 +0800

    Update get_codes_airport_and_names_from_tianxun.py

--- airportsweb/get_codes_airport_and_names_from_tianxun.py
+++ airportsweb/get_codes_airport_and_names_from_tianxun.py
@@ -5,7 +5,7 @@
 
 # In[19]:
 
-# 輸入
+# 导入os和json两个模块，打开data目录下的airport_zh.json，然后输出到数据。
 import os
 import json
 with open(os.path.join('data','airports_zh.json'), 'r', encoding='utf8') as fp:
@@ -13,18 +13,18 @@ with open(os.path.join('data','airports_zh.json'), 'r', encoding='utf8') as fp:
 
 
 # In[20]:
-
+#重新加载一次之前运行过的模块
 reload
 
 
 # In[21]:
-
+#把reload上的数据赋值到dict_c_n上
 # 讀回到dict_c_n 就不用再抓網頁
 dict_c_n = reload
 
 
 # In[22]:
-
+#导入csv模块，通过row函数等处理原airport-codes.csv得出新的数据
 # 數據來源
 # https://github.com/datasets/airport-codes
 import csv
@@ -38,41 +38,41 @@ with open(os.path.join('data','airport-codes.csv'), 'r', encoding='utf8') as fp:
 
 
 # In[23]:
-
+#输出data内的数据
 data
 
 
 # In[24]:
-
+#输出data数据中的值
 data.keys()
 
 
 # In[25]:
-
+#输出dict_c_n数据中的值
 dict_c_n.keys()
 
 
 # In[26]:
-
+#打印出dict_c_n和data中的数据总量
 # 比較兩數據的大小
 print (len(dict_c_n))
 print (len(data))
 
 
 # In[27]:
-
+#取dict_c_n的值和data的值的交集生成一个集合
 # 取交集
 airports_available = set(dict_c_n.keys()).intersection(set(data.keys()))
 
 
 # In[28]:
-
+#通过生成特定格式的的字典的办法赋值到data_output下
 data_output = { dict_c_n[c]:{'latitude':float(data[c].split(",")[0]),'longitude':float(data[c].split(",")[1]) }  for c in airports_available}
 data_output
 
 
 # In[29]:
-
+#导入json和os.path模块再输出保存到Airports_zh_code_geo.json下
 # 輸出
 import json
 import os.path
